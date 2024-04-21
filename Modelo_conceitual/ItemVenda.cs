537d066279ccb0252acab551ea9bb6903a174a41
Luana-F3RNANDA <cl203013@g.unicamp.br>
2024-04-20 21:25:10 -0300

    Item Venda

---
+++ Modelo_conceitual/ItemVenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo_conceitual
+{
+    public class ItemVenda
+    {
+        public int quantidade {  get; set; }
+        public decimal preco { get; set; }
+        public decimal desconto { get; set; }
+    }
+}
---
+++ Negocio/ItemVendaService.cs
@@ -0,0 +1,56 @@
+﻿using Dados;
+using Modelo_conceitual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ItemVendaService
+    {
+        private readonly ItemVendaRepository itemVendaRepository;
+
+        public ItemVendaService()
+        {
+            itemVendaRepository = new ItemVendaRepository();
+        }
+
+        public void CadastrarItemVenda(int quantidade, decimal preco, decimal desconto)
+        {
+            // Insira as validações e regras de negócio aqui
+            // Por exemplo, verificar se o email já está cadastrado
+
+            var itemVenda = new ItemVenda
+            {
+                quantidade = quantidade,
+                preco = preco,
+                desconto = desconto
+            };
+
+            itemVendaRepository.Adicionar(itemVenda);
+
+        }
+
+        public void CadastrarProduto(ItemVenda itemVenda)
+        {
+            // Insira as validações e regras de negócio aqui
+            // Por exemplo, verificar se o email já está cadastrado
+
+            itemVendaRepository.Adicionar(itemVenda);
+
+        }
+
+        public IEnumerable<ItemVenda> ObterTodos()
+        {
+            return itemVendaRepository.ObterTodos();
+        }
+        public List<ItemVenda> getAll()
+        {
+            return itemVendaRepository.ObterTodos().ToList<ItemVenda>();
+        }
+
+    }
+}
+
