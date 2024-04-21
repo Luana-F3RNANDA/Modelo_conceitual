0f69df6584c909d490365a4f16356af8cfd149df
Luana-F3RNANDA <cl203013@g.unicamp.br>
2024-04-21 15:53:29 -0300

    Item Compra

---
+++ Negocio/ItemCompraService.cs
@@ -0,0 +1,54 @@
+﻿using Modelo_conceitual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ItemCompraService
+    {
+        private readonly ItemCompraRepository itemCompraRepository;
+
+        public ItemCompraService()
+        {
+            itemCompraRepository = new ItemCompraRepository();
+        }
+
+        public void CadastrarItemCompra(int quantidade, decimal preco)
+        {
+            // Insira as validações e regras de negócio aqui
+            // Por exemplo, verificar se o email já está cadastrado
+
+            var itemCompra = new ItemCompra
+            {
+                quantidade = quantidade,
+                preco = preco,
+            };
+
+            itemCompraRepository.Adicionar(itemCompra);
+
+        }
+
+        public void CadastrarCompra(ItemCompra itemCompra)
+        {
+            // Insira as validações e regras de negócio aqui
+            // Por exemplo, verificar se o email já está cadastrado
+
+            itemCompraRepository.Adicionar(itemCompra);
+
+        }
+
+        public IEnumerable<ItemCompra> ObterTodos()
+        {
+            return itemCompraRepository.ObterTodos();
+        }
+        public List<ItemCompra> getAll()
+        {
+            return itemCompraRepository.ObterTodos().ToList<ItemCompra>();
+        }
+
+    }
+}
+
