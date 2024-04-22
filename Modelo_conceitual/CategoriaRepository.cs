e4c697cac831e5ff52384edcf27d5e53280a0087
HelenaPetruz <cl203253@g.unicamp.br>
2024-04-22 15:25:15 -0300

    categoria

---
+++ Modelo_conceitual/CategoriaRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dados;
+
+namespace Dados
+{
+        public class CategoriaRepository
+        {
+            private static List<Categoria> listaCategoria = new List<Categoria>();
+
+            public void Adicionar(Categoria categoria)
+            {
+                // Aqui você poderia inserir o cliente em um banco de dados
+                // Para simplificar, estamos apenas adicionando a uma lista em memória
+                listaCategoria.Add(categoria);
+            }
+
+            public IEnumerable<Categoria> ObterTodos()
+            {
+                return listaCategoria;
+            }
+        }
+}
---
+++ Negocio/CategoriaService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dados;
+
+namespace Negocio
+{
+    public class CategoriaService
+    {
+        private readonly CategoriaRepository categoriaRepository;
+
+        public CategoriaService()
+        {
+            categoriaRepository = new CategoriaRepository();
+        }
+
+        public void CadastrarCategoria(int id, string nome)
+        {
+            // Insira as validações e regras de negócio aqui
+            // Por exemplo, verificar se o email já está cadastrado
+
+            var categoria = new Categoria
+            {
+                id = id,
+                nome=nome,
+            };
+
+            categoriaRepository.Adicionar(categoria);
+
+        }
+
+        public void CadastrarCategoria(Categoria categoria)
+        {
+            // Insira as validações e regras de negócio aqui
+            // Por exemplo, verificar se o email já está cadastrado
+
+            categoriaRepository.Adicionar(categoria);
+
+        }
+
+        public IEnumerable<Categoria> ObterTodos()
+        {
+            return categoriaRepository.ObterTodos();
+        }
+        public List<Categoria> getAll()
+        {
+            return categoriaRepository.ObterTodos().ToList<Categoria>();
+        }
+
+    }
+}
