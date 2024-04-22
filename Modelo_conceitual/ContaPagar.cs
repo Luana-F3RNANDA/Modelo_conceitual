2d4d5d5fc669a55405919c58b1b1fbf1c8cefecc
HelenaPetruz <cl203253@m.unicamp.br>
2024-04-22 13:53:41 -0300

    ContaPagar

---
+++ Modelo_conceitual/ContaPagar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dados;
+
+namespace Dados { 
+    public class ContaPagar
+    {
+        public int id { get; set; }
+        public decimal valor { get; set; }
+        public DateTime dataCadastro {get; set;}
+        public DateTime dataVencimento { get; set; }
+        public DateTime dataPagamento { get; set; }
+        public MeioPagamento meioPagamento { get; set; }
+        public EstadoPagamento situacao { get; set; }
+
+
+    }
+}
